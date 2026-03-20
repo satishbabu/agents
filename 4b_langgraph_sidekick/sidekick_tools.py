41bda230f6f67006d516f16f7327d15706fbd4d7
Satish Babu <4342227+satishbabu@users.noreply.github.com>
2026-03-19 21:47:42 -0400

    Added sidekick project

---
+++ 4b_langgraph_sidekick/sidekick_tools.py
@@ -0,0 +1,41 @@
+from playwright.async_api import async_playwright
+from langchain_community.agent_toolkits import PlayWrightBrowserToolkit
+from dotenv import load_dotenv
+import os
+import requests
+from langchain_core.tools import Tool
+from langchain_community.agent_toolkits import FileManagementToolkit
+from langchain_community.tools.wikipedia.tool import WikipediaQueryRun
+from langchain_experimental.tools import PythonREPLTool
+from langchain_community.utilities import GoogleSerperAPIWrapper
+from langchain_community.utilities.wikipedia import WikipediaAPIWrapper
+
+load_dotenv(override=True)
+
+serper = GoogleSerperAPIWrapper()
+
+async def playwright_tools():
+    playwright = await async_playwright().start()
+    browser = await playwright.chromium.launch(headless=False)
+    toolkit = PlayWrightBrowserToolkit.from_browser(async_browser=browser)
+    return toolkit.get_tools(), browser, playwright
+
+def get_file_tools():
+    toolkit = FileManagementToolkit(root_dir="sandbox")
+    return toolkit.get_tools()
+
+async def other_tools():
+    file_tools = get_file_tools()
+
+    tool_search =Tool(
+        name="search",
+        func=serper.run,
+        description="Use this tool when you want to get the results of an online web search"
+    )
+
+    wikipedia = WikipediaAPIWrapper()
+    wiki_tool = WikipediaQueryRun(api_wrapper=wikipedia)
+
+    python_repl = PythonREPLTool()
+    
+    return file_tools + [tool_search, python_repl,  wiki_tool]
